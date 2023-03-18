7821b792bb5f3e760419a8eb5bf1f1f59f67af5e
Vighnesh Ramasamy <vighneshramasamy@gmail.com>
2023-03-18 22:56:31 +0800

    Formatting

--- MS_Testing/LISTING_MS/app.py
+++ MS_Testing/LISTING_MS/app.py
@@ -21,30 +21,31 @@ def index():
 
 @app.route('/add_product', methods=['POST'])
 def add_product():
-      itemName = request.form['itemName']
-      quantity = request.form['quantity']
-      price = request.form['price']
-      dateOfPurchase = request.form['datOfPurchase']
-      availability = request.form['availability']
 
-      collection.insert_one({'itemName': itemName, 'quantity': quantity, 'price': price, 'dateOfPurchase': dateOfPurchase, 'availability': availability})
-      return jsonify(collection)
+	itemName = request.form['itemName']
+	quantity = request.form['quantity']
+	price = request.form['price']
+	dateOfPurchase = request.form['datOfPurchase']
+	availability = request.form['availability']
+
+	collection.insert_one({'itemName': itemName, 'quantity': quantity, 'price': price, 'dateOfPurchase': dateOfPurchase, 'availability': availability})
+	return jsonify(collection)
 
 @app.route('/edit/<product_id>', methods=['POST'])
 def edit_product(product_id):
-      product = collection.find_one({'_id': ObjectId(product_id)})
-      itemName = request.form['itemName']
-      quantity = request.form['quantity']
-      price = request.form['price']
-      dateOfPurchase = request.form['datOfPurchase']
-      availability = request.form['availability']
-      collection.update_one({'_id': ObjectId(product_id)}, {'$set': {'itemName': itemName, 'quantity': quantity, 'price': price, 'dateOfPurchase': dateOfPurchase, 'availability': availability}})
-      return jsonify(collection)
+	product = collection.find_one({'_id': ObjectId(product_id)})
+	itemName = request.form['itemName']
+	quantity = request.form['quantity']
+	price = request.form['price']
+	dateOfPurchase = request.form['datOfPurchase']
+	availability = request.form['availability']
+	collection.update_one({'_id': ObjectId(product_id)}, {'$set': {'itemName': itemName, 'quantity': quantity, 'price': price, 'dateOfPurchase': dateOfPurchase, 'availability': availability}})
+	return jsonify(collection)
 
 @app.route('/delete/<product_id>')
 def delete_product(product_id):
-      collection.delete_one({'_id': ObjectId(product_id)})
-      return jsonify(collection)
+	collection.delete_one({'_id': ObjectId(product_id)})
+	return jsonify(collection)
 
 if __name__ == '__main__':
       app.run(debug=True)
