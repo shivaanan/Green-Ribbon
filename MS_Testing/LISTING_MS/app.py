c0b9c569d0a7665fd59b3093c1a5c982abd2686c
L1uM4rt1n <96768015+L1uM4rt1n@users.noreply.github.com>
2023-03-20 13:23:32 +0800

    finalised app.py

--- MS_Testing/LISTING_MS/app.py
+++ MS_Testing/LISTING_MS/app.py
@@ -27,8 +27,6 @@ def getAllProducts():
 
 @app.route('/add_product', methods=['POST'])
 def add_product():
-	
-
 	productID = get_next_sequence_value("productid")
 	# -----------------------------------------------------------------------------
 	# testing using postman body
@@ -57,23 +55,27 @@ def get_next_sequence_value(sequence_name):
 	sequence_value = counter.find_one_and_update({"_id": sequence_name}, {"$inc": {"seq": 1}})
 	return sequence_value['seq']
 
-# not needed if i not wrong - not in scenario
-# @app.route('/edit/<product_id>', methods=['POST'])
-# def edit_product(product_id):
-# 	product = collection.find_one({'_id': ObjectId(product_id)})
-# 	itemName = request.form['itemName']
-# 	quantity = request.form['quantity']
-# 	price = request.form['price']
-# 	dateOfPurchase = request.form['datOfPurchase']
-# 	availability = request.form['availability']
-# 	collection.update_one({'_id': ObjectId(product_id)}, {'$set': {'itemName': itemName, 'quantity': quantity, 'price': price, 'dateOfPurchase': dateOfPurchase, 'availability': availability}})
-# 	return jsonify(collection)
+## editing the item quantity in the 
+@app.route('/edit/<product_id>', methods=['GET'])
+def edit_product(product_id, soldQuantity):
+	product = collection.find({_id: product_id})
+	data = request.get_json()
+	itemName = data["itemName"]
+	price = data['price']
+	dateOfPost = data['dateOfPost']
+	availability = data['availability']
+	imgURL = data["imgURL"]
+	quantity = data['quantity'] - soldQuantity
 
-# not needed if i not wrong - not in scenario
-# @app.route('/delete/<product_id>')
-# def delete_product(product_id):
-# 	collection.delete_one({'_id': ObjectId(product_id)})
-# 	return jsonify(collection)
+
+	collection.update_one({'productID' : product_id,'itemName': itemName, 'quantity': quantity, 'price': price, 'dateOfPost': dateOfPost, 'availability': availability, "imgURL" : imgURL})
+	return getAllProducts()
+
+## removing sold OR no quantity products
+@app.route('/delete/<product_id>')
+def delete_product(product_id):
+	collection.delete_one({_id: product_id})
+	return getAllProducts()
 
 if __name__ == '__main__':
       app.run(debug=True)
