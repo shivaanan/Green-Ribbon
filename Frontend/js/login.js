93d65d84dff251e9f29ab216ad0d365a4b856d96
Vighnesh Ramasamy <vighneshramasamy@gmail.com>
2023-03-22 10:56:44 +0800

    added sign in function
    sign in function makes a request to account MS

--- Frontend/js/login.js
+++ Frontend/js/login.js
@@ -1,3 +1,4 @@
+// -------------- Shifting between sign in and sign up tab -------------------------
 const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
@@ -9,23 +10,46 @@ signUpButton.addEventListener('click', () => {
 signInButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
 });
+// ---------------------------------------------------------------------------------
 
-function signIn() {
-	document.getElementById("error").innerHTML = ""
+
+// -------------- sign in function  ------------------------------------------------
+ function signIn() {
+	// document.getElementById("error").innerHTML = ""
+	console.log("test signin button");
 	
-	if (!validateSignIn()) {
-		document.getElementById("error").innerHTML = "erorrr"
-	}
+	let email = document.getElementById("username").value
+	let password = document.getElementById("password").value
+
+	console.log(email);
+	console.log(password);
+
+	console.log("-------In user signIn------");
+        
+	axios.post('http://127.0.0.1:5000/loginuser', {
+		"email":  email,
+		"password" : password
+		})
+		.then(response => {
+			// console.log(response.data);
+			console.log(response.data["success"]);
+
+			if (response.data["success"]) {
+				// Redirect to home page
+				window.location.href = "home.html";
+			  } else {
+				// Show error message
+				document.getElementById("error").innerHTML = "Invalid email or password";
+			  }
+			
+		})
+		
+		.catch(error => {
+			
+		});
+
+	console.log("-------end user signIn------");
 }
+// ---------------------------------------------------------------------------------
 
-function validateSignIn() {
-	var username = document.getElementById("username").value;
-	var pw = document.getElementById("password").value;
-	console.log(username)
 
-	if (username == "" || pw == "") {
-		return false
-	}
-	return true
-
-}
