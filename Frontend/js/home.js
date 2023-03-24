b5a9afe5653654c52c4d2ce707c6ee0dc103e27a
Vighnesh Ramasamy <vighneshramasamy@gmail.com>
2023-03-25 00:40:42 +0800

    quantity input functions
    increment, decrement  functionality

--- Frontend/js/home.js
+++ Frontend/js/home.js
@@ -1,138 +1,77 @@
-// var cartCount = 0,
-// 	buy = $(".btn"),
-// 	span = $(".number"),
-// 	cart = $(".cart"),
-// 	quickview = $(".quickviewContainer"),
-// 	quickViewBtn = $(".quickview"),
-// 	close = $(".quickviewContainer .close"),
-// 	minicart = [],
-// 	totalPrice = [],
-// 	miniCartPrice;
 
-// buy.on("click", addToCart);
-// quickViewBtn.on("click", quickView);
-// cart.on("click", showMiniCart);
-// close.on("click", function () {
-// 	quickview.removeClass("active");
-// });
-
-// function quickView() {
-// 	var description = $(this).parent().find(".description").text(),
-// 		header = $(this).parent().find(".header").text(),
-// 		price = $(this).find(".price"),
-// 		quickViewHeader = $(".quickviewContainer .headline"),
-// 		quickViewDescription = $(".quickviewContainer .description");
-// 	clearTimeout(timeQuick);
-// 	if (quickview.hasClass("active")) {
-// 		quickview.removeClass("active");
-// 		var timeQuick = setTimeout(function () {
-// 			quickview.addClass("active");
-// 		}, 300);
-// 	} else {
-// 		quickview.addClass("active");
-// 	}
-
-// 	quickViewHeader.text(header);
-// 	quickViewDescription.text(description);
-// }
-
-// function showMiniCart() {
-// 	$(".mini").toggleClass("visible");
-// }
-
-// function addToCart() {
-// 	var self = $(this),
-// 		productName = $(this).parent().find(".header").text(),
-// 		miniCartNames = $(".products"),
-// 		names = $(".names"),
-// 		price = $(this).parent().find(".price").text(),
-// 		priceInt = parseInt(price);
-
-// 	totalPrice.push(priceInt);
-// 	miniCartPrice = totalPrice.reduce(function (a, b) {
-// 		return a + b;
-// 	});
-// 	$(".miniprice").text("Total amount: " + miniCartPrice + ",-");
-// 	minicart.push(productName);
-// 	lastProduct = minicart[minicart.length - 1];
-// 	miniCartNames.text("Your cart lines: ");
-// 	names.append("<p>" + lastProduct + "</p>");
-
-// 	cartCount++;
-// 	span.text(cartCount);
-// 	clearTimeout(time);
-// 	if (span.hasClass("update")) {
-// 		span.removeClass("update");
-// 		span.addClass("updateQuantity");
-// 		var time = setTimeout(function () {
-// 			span.removeClass("updateQuantity");
-// 			span.addClass("update");
-// 		}, 700);
-// 	} else {
-// 		span.addClass("update");
-// 	}
-// 	if (cartCount == 1) {
-// 		cart.toggleClass("icon-basket icon-basket-loaded");
-// 	}
-
-// 	$(this).addClass("ok");
-// 	var timeOk = setTimeout(function () {
-// 		self.removeClass("ok");
-// 	}, 1000);
-// }
 
 const homePage = Vue.createApp({
-  data() {
-    return {
-      products: [],
-    };
-  }, // data
+    data() {
+        return {
+            products: [],
+        };
+    }, // data
 
-  mounted() {
-    console.log("-------In user mounted------");
+    mounted() {
+        console.log("-------In user mounted------");
 
-    axios
-      .get("http://127.0.0.1:5001/products")
-      .then((response) => {
-        console.log("hi");
-        console.log(response.data[0]);
-        this.products = response.data;
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+        axios
+            .get("http://127.0.0.1:5001/products")
+            .then((response) => {
+                console.log("hi");
+                console.log(response.data[0]);
+                this.products = response.data;
+            })
+            .catch((error) => {
+                console.log(error);
+            });
 
-    console.log("-------end user  mounted------");
-  },
-
-  computed: {},
-
-  methods: {
-    async buyNow(productID) {
-      console.log(productID);
-
-      try {
-        const response = await axios.post(
-          "http://127.0.0.1:5001/create-checkout-session",
-          {
-            product_id: productID,
-          }
-        );
-
-        const data = response.data;
-
-        if (data.checkout_session_id) {
-          const stripe = Stripe(data.checkout_public_key);
-          stripe.redirectToCheckout({ sessionId: data.checkout_session_id });
-        } else {
-          // Handle errors (e.g., Price not found for the given product)
-          console.error("Error:", data.error);
-        }
-      } catch (error) {
-        console.error("Error:", error);
-      }
+        console.log("-------end user  mounted------");
     },
-  },
+
+    computed: {},
+
+    methods: {
+        async buyNow(productID) {
+            console.log(productID);
+
+            try {
+                const response = await axios.post(
+                    "http://127.0.0.1:5001/create-checkout-session",
+                    {
+                        product_id: productID,
+                    }
+                );
+
+                const data = response.data;
+
+                if (data.checkout_session_id) {
+                    const stripe = Stripe(data.checkout_public_key);
+                    stripe.redirectToCheckout({ sessionId: data.checkout_session_id });
+                } else {
+                    // Handle errors (e.g., Price not found for the given product)
+                    console.error("Error:", data.error);
+                }
+            } catch (error) {
+                console.error("Error:", error);
+            }
+        },
+
+        incrementQuantity(productID) {
+            let qtyInput = document.getElementById(productID ).value;
+    
+            qtyInput = parseInt(qtyInput) + 1;
+
+            document.getElementById(productID).value = qtyInput;
+        },   
+
+        decrementQuantity(productID) {
+            let qtyInput = document.getElementById(productID ).value;
+         
+            if (parseInt(qtyInput) == 0) {
+                return;
+            }
+
+            qtyInput = parseInt(qtyInput) - 1;
+
+            document.getElementById(productID).value = qtyInput;
+        },   
+    },
 });
 
 const vm = homePage.mount("#homePage");
