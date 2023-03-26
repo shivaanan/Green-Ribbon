b21a7b1ff8ebe211100f1e15d03a906aaf935b80
MrTaooo <lintao199@gmail.com>
2023-03-26 18:35:11 +0800

    Updated payment MS, Adjusted Layouts and added new parameters.

--- Frontend/js/checkout.js
+++ Frontend/js/checkout.js
@@ -1,5 +1,6 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const publicKey = 'pk_test_51MltK7EBOpB8WMsEafwEYrSYcLFCnAasAwZceaxQYgfYrZCxiqFymPFqCAhtz4BL0L7XB1HwKWzK53blzlcakXAj00b5LtAwQQ'; // Replace with your Stripe public key
+document.addEventListener("DOMContentLoaded", () => {
+  const publicKey =
+    "pk_test_51MltK7EBOpB8WMsEafwEYrSYcLFCnAasAwZceaxQYgfYrZCxiqFymPFqCAhtz4BL0L7XB1HwKWzK53blzlcakXAj00b5LtAwQQ"; // Replace with your Stripe public key
 
   const stripe = Stripe(publicKey);
   const elements = stripe.elements();
@@ -7,41 +8,59 @@ document.addEventListener('DOMContentLoaded', () => {
   card.mount("#card-element");
 
   const paymentForm = document.getElementById("payment-form");
-  paymentForm.addEventListener('submit', async (event) => {
-      event.preventDefault();
+  paymentForm.addEventListener("submit", async (event) => {
+    event.preventDefault();
 
-      const response = await fetch('http://127.0.0.1:8000/create_payment_intent', {
-          method: 'POST',
-          headers: {
-              'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-              productID: 0,
-              quantity: 2, // or get this value from a form input
-          }),
-      });
-
-      if (!response.ok) {
-          const errorData = await response.json();
-          console.error('Backend error:', errorData.error);
-          return;
+    const response = await fetch(
+      "http://127.0.0.1:8000/create_payment_intent",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          productID: 0,
+          quantity: 2, // or get this value from a form input
+        }),
       }
+    );
 
-      const data = await response.json();
-      const result = await stripe.confirmCardPayment(data.clientSecret, {
-          payment_method: {
-              card: card,
-              billing_details: {
-                  name: 'Your Name', // Replace with a form input for the user's name
-              },
-          },
-      });
+    if (!response.ok) {
+      const errorData = await response.json();
+      console.error("Backend error:", errorData.error);
+      return;
+    }
 
-      if (result.error) {
-          console.error('Payment failed:', result.error.message);
-      } else {
-          console.log('Payment succeeded:', result.paymentIntent.id);
-          window.location.href = 'thanks.html'; // Redirect to thanks.html after successful payment
-      }
+    const data = await response.json();
+    // Card Holder Name
+    const cardHolderName = document.getElementById('cardHolderName').value
+    const result = await stripe.confirmCardPayment(data.clientSecret, {
+      payment_method: {
+        card: card,
+        billing_details: {
+          name: cardHolderName, // Replace with a form input for the user's name
+        },
+      },
+    });
+
+    if (result.error) {
+      console.error("Payment failed:", result.error.message);
+    } else {
+      console.log("Payment succeeded:", result.paymentIntent.id);
+      window.location.href = "thanks.html"; // Redirect to thanks.html after successful payment
+    }
+
+    const errorMessageElement = document.getElementById("error-message");
+
+    // ...
+    if (result.error) {
+      console.error("Payment failed:", result.error.message);
+      errorMessageElement.textContent = result.error.message;
+      errorMessageElement.style.display = "block";
+    } else {
+      console.log("Payment succeeded:", result.paymentIntent.id);
+      errorMessageElement.style.display = "none";
+      window.location.href = "thanks.html"; // Redirect to thanks.html after successful payment
+    }
   });
 });
--- MS_Testing/paymentMS/payment.py
+++ MS_Testing/paymentMS/payment.py
@@ -1,71 +1,54 @@
-from flask import Flask, request, jsonify, send_from_directory
-from flask_cors import CORS
-from datetime import datetime
-
 import stripe
-import requests
-
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
-app.config['STRIPE_PUBLIC_KEY'] = 'pk_test_51MltK7EBOpB8WMsEafwEYrSYcLFCnAasAwZceaxQYgfYrZCxiqFymPFqCAhtz4BL0L7XB1HwKWzK53blzlcakXAj00b5LtAwQQ'
-app.config['STRIPE_SECRET_KEY'] = 'sk_test_51MltK7EBOpB8WMsEGsB51xLyAgs77LcOmFOr8mmzF2cPB0Fb0TeKXRRcv24nI6GId4wSEWovYyvWfx8iL2SmKpP800dNlhECw0'
-
-stripe.api_key = app.config['STRIPE_SECRET_KEY']
+# Replace with your own Stripe API keys
+stripe.api_key = 'sk_test_51MltK7EBOpB8WMsEGsB51xLyAgs77LcOmFOr8mmzF2cPB0Fb0TeKXRRcv24nI6GId4wSEWovYyvWfx8iL2SmKpP800dNlhECw0'
 
 
-@app.route('/create-checkout-session', methods=['POST'])
-def create_checkout_session():
-    product = request.json
+PRODUCTS = {
+    0: {
+        'itemName': 'Chair',
+        'price': '2',
+        'productID': 0,
+        'quantity': 8
+    }
+}
+
+@app.route('/create_payment_intent', methods=['POST'])
+def create_payment_intent():
+    data = request.get_json()
+    product_id = data.get('productID')
+    quantity = data.get('quantity', 1)
+    print(data)
+
+    if product_id not in PRODUCTS:
+        return jsonify({'error': 'Invalid product ID'}), 400
+
+    product = PRODUCTS[product_id]
+
+    # Calculate the total amount
+    amount = int(product['price']) * quantity * 100  # Stripe expects the amount in cents
 
     try:
-
-        # Convert price to cents
-        product_price = int(float(product['price']) * 100)
-
-        # Create a new Stripe checkout session
-        checkout_session = stripe.checkout.Session.create(
-            payment_method_types=['card'],
-            line_items=[{
-                'price_data': {
-                    'currency': 'usd',  # Change this to your desired currency
-                    'product_data': {
-                        'name': product['itemName'],
-                    },
-                    'unit_amount': product_price,
-                },
-                'quantity': 1,
-            }],
-            mode='payment',
-            success_url=request.url_root + 'thanks',
-            cancel_url=request.url_root + 'home',
+        # Create a new PaymentIntent
+        payment_intent = stripe.PaymentIntent.create(
+            amount=amount,
+            currency='usd',
+            description=f"{product['itemName']} x{quantity}",
         )
-        
-        dateOfPurchase = datetime.now()
         return jsonify({
-            "code": 201,
-            "data": {
-                'dateOfPurchase': dateOfPurchase,
-                'itemPurchase': product,
-                'checkout_session_id': checkout_session.id,
-                'checkout_public_key': app.config['STRIPE_PUBLIC_KEY'],
-            }
+            'clientSecret': payment_intent['client_secret'],
+            'description': payment_intent['description']
         })
 
     except Exception as e:
+        print("Error:", e)
+        print(traceback.format_exc())
         return jsonify({'error': str(e)}), 400
-
-
-@app.route('/thanks')
-def thanks():
-    return send_from_directory('../../Frontend', 'thanks.html')
-
-
-@app.route('/home')
-def home():
-    return send_from_directory('../../Frontend', 'home.html')
-
 
 if __name__ == '__main__':
     app.run(port=5002, debug=True)
