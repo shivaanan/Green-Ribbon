7fd11963e630794d98bf77fa654d2057fae10673
GohCheeKiat <40667778+GohCheeKiat@users.noreply.github.com>
2023-03-29 21:04:05 +0800

    Update Location.java

--- MS_Testing/Location_MS/Location.java
+++ MS_Testing/Location_MS/Location.java
@@ -1,7 +1,6 @@
 package com.is213.project;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -9,70 +8,125 @@ import java.net.URLEncoder;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-public class GetLocation {
+public class Locationdist {
   public static void main(String[] args) {
     try {
-        // Get the user's IP address
-        URL ipApiUrl = new URL("https://api.ipify.org");
-        HttpURLConnection ipApiCon = (HttpURLConnection) ipApiUrl.openConnection();
-        BufferedReader ipApiIn = new BufferedReader(new InputStreamReader(ipApiCon.getInputStream()));
-        String ipAddress = ipApiIn.readLine();
-        ipApiIn.close();
+      // Get the user's IP address
+      URL ipApiUrl = new URL("https://api.ipify.org");
+      HttpURLConnection ipApiCon = (HttpURLConnection) ipApiUrl.openConnection();
+      BufferedReader ipApiIn = new BufferedReader(new InputStreamReader(ipApiCon.getInputStream()));
+      String ipAddress = ipApiIn.readLine();
+      ipApiIn.close();
 
-        String ipInfoUrl = "https://ipinfo.io/" + ipAddress + "/json?token=63daa13dc0940f"; // Replace YOUR_TOKEN_HERE with your actual API token
+      String ipInfoUrl = "https://ipinfo.io/" + ipAddress + "/json?token=63daa13dc0940f";
 
-        URL ipInfoObj = new URL(ipInfoUrl);
-        HttpURLConnection ipInfoCon = (HttpURLConnection) ipInfoObj.openConnection();
+      URL ipInfoObj = new URL(ipInfoUrl);
+      HttpURLConnection ipInfoCon = (HttpURLConnection) ipInfoObj.openConnection();
 
-        BufferedReader ipInfoIn = new BufferedReader(new InputStreamReader(ipInfoCon.getInputStream()));
-        String ipInfoInputLine;
-        StringBuffer ipInfoResponse = new StringBuffer();
+      BufferedReader ipInfoIn = new BufferedReader(new InputStreamReader(ipInfoCon.getInputStream()));
+      String ipInfoInputLine;
+      StringBuffer ipInfoResponse = new StringBuffer();
 
-        while ((ipInfoInputLine = ipInfoIn.readLine()) != null) {
-            ipInfoResponse.append(ipInfoInputLine);
+      while ((ipInfoInputLine = ipInfoIn.readLine()) != null) {
+        ipInfoResponse.append(ipInfoInputLine);
+      }
+      ipInfoIn.close();
+
+     
+      JSONObject ipInfoJsonObj = new JSONObject(ipInfoResponse.toString());
+      String latLng = ipInfoJsonObj.getString("loc");
+
+      // Use a geocoding API to get the user's location
+      String geocodingUrl = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + latLng + "&key=AIzaSyCItPqAhCSJVc13yxvnZoHb7SyTajxJWJ8";
+
+      URL geocodingObj = new URL(geocodingUrl);
+      HttpURLConnection geocodingCon = (HttpURLConnection) geocodingObj.openConnection();
+
+      BufferedReader geocodingIn = new BufferedReader(new InputStreamReader(geocodingCon.getInputStream()));
+      String geocodingInputLine;
+      StringBuffer geocodingResponse = new StringBuffer();
+
+      while ((geocodingInputLine = geocodingIn.readLine()) != null) {
+        geocodingResponse.append(geocodingInputLine);
+      }
+      geocodingIn.close();
+
+
+      JSONObject geocodingJsonObj = new JSONObject(geocodingResponse.toString());
+      if (geocodingJsonObj.has("results")) {
+        JSONArray results = geocodingJsonObj.getJSONArray("results");
+        JSONObject location = results.getJSONObject(0).getJSONObject("geometry").getJSONObject("location");
+
+      
+        double lat1 = location.getDouble("lat");
+        double lng1 = location.getDouble("lng");
+
+        // Get Destination address and convert to LatLng for calculation. Change this for dynamic checking of location distance
+        String address = "2 Handy Rd, #01-21 The Assembly Ground, The Cathay (Dhoby Ghaut), The Cathay, Singapore 229233";
+
+       
+        String urlString = "https://maps.googleapis.com/maps/api/geocode/json?address=" + URLEncoder.encode(address, "UTF-8") + "&key=AIzaSyCItPqAhCSJVc13yxvnZoHb7SyTajxJWJ8";
+
+   
+        URL url = new URL(urlString);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("Accept", "application/json");
+        BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+
+     
+        StringBuilder sb = new StringBuilder();
+        String output;
+        while ((output = br.readLine()) != null) {
+            sb.append(output);
         }
-        ipInfoIn.close();
+        conn.disconnect();
+        JSONObject response = new JSONObject(sb.toString());
+        JSONArray results2 = response.getJSONArray("results");
+        JSONObject result2 = results2.getJSONObject(0);
+        JSONObject location2 = result2.getJSONObject("geometry").getJSONObject("location");
+        double destLat = location2.getDouble("lat"); 
+        double destLng = location2.getDouble("lng");
+     
+        String distanceUrl = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=" + lat1 + "," + lng1 + "&destinations=" + destLat + "," + destLng + "&mode=driving" + "&key=AIzaSyCItPqAhCSJVc13yxvnZoHb7SyTajxJWJ8"; 
 
-        // Parse the JSON response to get the latitude and longitude values
-        JSONObject ipInfoJsonObj = new JSONObject(ipInfoResponse.toString());
-        String latLng = ipInfoJsonObj.getString("loc");
+        URL distanceObj = new URL(distanceUrl);
+        HttpURLConnection distanceCon = (HttpURLConnection) distanceObj.openConnection();
 
-        
-        // Use a geocoding API to get the user's location
-        String url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + latLng + "&key=AIzaSyCItPqAhCSJVc13yxvnZoHb7SyTajxJWJ8"; // Replace YOUR_API_KEY_HERE with your actual API key
+        BufferedReader distanceIn = new BufferedReader(new InputStreamReader(distanceCon.getInputStream()));
+        String distanceInputLine;
+        StringBuffer distanceResponse = new StringBuffer();
 
-        URL obj = new URL(url);
-        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-
-        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-        String inputLine;
-        StringBuffer response = new StringBuffer();
-
-        while ((inputLine = in.readLine()) != null) {
-            response.append(inputLine);
+        while ((distanceInputLine = distanceIn.readLine()) != null) {
+            distanceResponse.append(distanceInputLine);
+            System.out.println(distanceResponse.toString()); //prints the whole response 
         }
-        in.close();
+        distanceIn.close();
 
-        // Parse the JSON response
-        JSONObject jsonObj = new JSONObject(response.toString());
-        System.out.println(jsonObj);
-        if (jsonObj.has("results")) {
-            JSONArray results = jsonObj.getJSONArray("results");
-            JSONObject location = results.getJSONObject(0).getJSONObject("geometry").getJSONObject("location");
-
-            // Get the latitude and longitude values
-            double lat = location.getDouble("lat");
-            double lng = location.getDouble("lng");
-
-            // Print the latitude and longitude values
-            System.out.println("Latitude: " + lat);
-            System.out.println("Longitude: " + lng);
+      
+        JSONObject distanceJsonObj = new JSONObject(distanceResponse.toString());
+        JSONArray distanceRows = distanceJsonObj.getJSONArray("rows");
+        JSONObject distanceElements = distanceRows.getJSONObject(0).optJSONArray("elements").optJSONObject(0);
+        if(distanceElements != null && distanceElements.has("distance")) {
+            JSONObject distanceJsonObject = distanceElements.getJSONObject("distance");
+            JSONObject durationJsonObject = distanceElements.getJSONObject("duration");
+            if (distanceJsonObject.has("text")) {
+                String distanceText = distanceJsonObject.getString("text");
+                String durationText = durationJsonObject.getString("text");
+                System.out.println("Distance: " + distanceText );
+                System.out.println("Duration: " + durationText + " by driving");
+            } else {
+                System.out.println("Error: Failed to get distance value from JSON response.");
+            }
         } else {
-            System.out.println("No results found.");
+            System.out.println("Error: Failed to get distance value from JSON response.");
         }
-    } catch (Exception e) {
+        
+    } else {
+        System.out.println("No results found.");
+    }
+    }   catch (Exception e) {
         e.printStackTrace();
     }
-  }
 }
-
+}
